d751b3351a5f9492ba89b6233a0c8c70d90c9982
Mike Crumby <crumbym@quickprog.com>
2015-09-13 04:36:34 -0700

    Added helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  helloworld.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  09/13/2015 04:02:40 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  MIKE CRUMBY (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;    
+}
+
